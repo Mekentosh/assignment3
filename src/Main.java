e2b936eb7728636d87c702ba41f67cb6ab19ce85
Mekentosh <m87022928044@gmail.com>
2023-10-23 12:41:15 +0600

    Main class не закомитился

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        FahrenheitCelsiusConverter converter = new FahrenheitCelsiusConverter();
+        TemperatureConverter kelvinAdapter = new KelvinAdapter(converter);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        double celsius = 25;
+        double fahrenheit = converter.convertToFahrenheit(celsius);
+        double kelvin = kelvinAdapter.convertToCelsius(fahrenheit);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(celsius + " Celsius = " + fahrenheit + " Fahrenheit");
+        System.out.println(fahrenheit + " Fahrenheit = " + kelvin + " Kelvin");
     }
 }
